ed8ef5bc028a4f9600f802d2155adb0ed78d2a8a
root <root@romudock.32xjcqscqnmufmfjyqsigr2vgh.bx.internal.cloudapp.net>
2022-06-22 06:58:26 +0000

    Docker:Using Python file - CMD and Entrypoint Together

---
+++ pytest/hello.py
@@ -0,0 +1,4 @@
+print("Hello !! Welcome to python script")
+for i in range(10):
+    print(i)
+print("Closing the session")
---
+++ pytest/pyloop.py
@@ -0,0 +1,3 @@
+print("Hello world")
+names = {'Amit':1,'Gopal':2}
+print("Dictionary items: " + names.items())
