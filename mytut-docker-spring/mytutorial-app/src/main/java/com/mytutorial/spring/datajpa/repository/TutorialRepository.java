a3f1b6b2dc869839269694b2299fde0cce26a999
root <root@romudock.32xjcqscqnmufmfjyqsigr2vgh.bx.internal.cloudapp.net>
2022-06-23 10:05:41 +0000

    Docker: Creating multi build docker compose application - Spring Boot

---
+++ mytut-docker-spring/mytutorial-app/src/main/java/com/mytutorial/spring/datajpa/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.mytutorial.spring.datajpa.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mytutorial.spring.datajpa.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean published);
+	List<Tutorial> findByTitleContaining(String title);
+}
